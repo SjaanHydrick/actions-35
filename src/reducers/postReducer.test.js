5e94954879fb895f22eab918f92ccd9a5b90d275
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-17 14:00:53 -0800

    reducer tests written and passing

---
+++ src/reducers/postReducer.test.js
@@ -0,0 +1,34 @@
+import { createPost, deletePost } from '../actions/postActions';
+import reducer from './postReducer';
+
+describe('post reducer', () => {
+  it('add a post with CREATE_POST action', () => {
+    const state = {
+      posts: []
+    };
+
+    const action = createPost({
+      id: 1,
+      title: 'blah',
+      body: 'blahblahblah'
+    });
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      posts:[{ id: 1, title: 'blah', body: 'blahblahblah' }]
+    });
+  });
+
+  it('delete a post with DELETE_POST action', () => {
+    const state = {
+      posts: [{ id: 1, title: 'blah', body: 'blahblahblah' }]
+    };
+
+    const action = deletePost(1);
+
+    expect(reducer(state, action)).toEqual({
+      posts: []
+    });
+  });
+});
