1b9a5205fd3c7240fae31840d840c238349fccbc
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-18 14:02:15 -0800

    corrected Reducers

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -5,7 +5,7 @@ export const initialState = {
   comments: []
 };
 
-export default function reducer(state, action) {
+export default function reducer(state = initialState, action) {
   switch(action.type) {
     case CREATE_COMMENT:
       return {
