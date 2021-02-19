7f80eabb6c5a8d15596dd2d37b76db4300e4e276
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-18 16:32:12 -0800

    commentReducer updated with UPDATE_COMMENT_ID

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,8 +1,9 @@
 /* eslint-disable max-len */
-import { CREATE_COMMENT, DELETE_COMMENT } from '../actions/commentActions';
+import { CREATE_COMMENT, DELETE_COMMENT, UPDATE_COMMENT_ID } from '../actions/commentActions';
 
 export const initialState = {
-  comments: []
+  comments: [],
+  commentId: 0
 };
 
 export default function reducer(state = initialState, action) {
@@ -16,6 +17,11 @@ export default function reducer(state = initialState, action) {
       return {
         ...state,
         comments: state.comments.filter(comment => comment.id !== action.payload)
+      };
+    case UPDATE_COMMENT_ID:
+      return {
+        ...state,
+        commentId: action.payload
       };
     default:
       return state;
